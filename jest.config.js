b2db4e9347ceb65ebc49ceaeb817d787bb568bc5
Wiktor Malavasi <malavasiwixus@gmail.com>
2022-02-08 15:04:07 +0100

    Test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} huehueheu */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
